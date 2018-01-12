99aac922bfc2694e6730f5cffe3342ad07861811
Thiago Paraizo <thiagoparaizo@gmail.com>
2018-01-12 00:35:05 -0300

    Ajuste app via ionic creator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import { FavoritosPage } from '../pages/favoritos/favoritos';
+import { MeuPerfilPage } from '../pages/meu-perfil/meu-perfil';
+import { VocePage } from '../pages/voce/voce';
+import { ContatoFutevoleiPage } from '../pages/contato-futevolei/contato-futevolei';
+import { InformaEsDeUsoPage } from '../pages/informa-es-de-uso/informa-es-de-uso';
+
+
+import { HomeTabPage } from '../pages/home-tab/home-tab';
+
+
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  @ViewChild(Nav) navCtrl: Nav;
+    rootPage:any = HomeTabPage ; 
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -18,5 +28,25 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+  goToHomeTab(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(HomeTabPage);
+  }
+  goToFavoritos(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(FavoritosPage);
+  }goToMeuPerfil(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(MeuPerfilPage);
+  }goToVoce(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(VocePage);
+  }goToContatoFutevolei(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(ContatoFutevoleiPage);
+  }goToInformaEsDeUso(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(InformaEsDeUsoPage);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { BuscaPage } from '../pages/busca/busca';
+import { FavoritosPage } from '../pages/favoritos/favoritos';
+import { DestaquesPage } from '../pages/destaques/destaques';
+import { HomeTabPage } from '../pages/home-tab/home-tab';
+import { MeuPerfilPage } from '../pages/meu-perfil/meu-perfil';
+import { VocePage } from '../pages/voce/voce';
+import { ContatoFutevoleiPage } from '../pages/contato-futevolei/contato-futevolei';
+import { InformaEsDeUsoPage } from '../pages/informa-es-de-uso/informa-es-de-uso';
+import { LoginPage } from '../pages/login/login';
+import { CadastroUsuarioPage } from '../pages/cadastro-usuario/cadastro-usuario';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,10 +20,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    BuscaPage,
+    FavoritosPage,
+    DestaquesPage,
+    HomeTabPage,
+    MeuPerfilPage,
+    VocePage,
+    ContatoFutevoleiPage,
+    InformaEsDeUsoPage,
+    LoginPage,
+    CadastroUsuarioPage
   ],
   imports: [
     BrowserModule,
@@ -26,10 +38,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    BuscaPage,
+    FavoritosPage,
+    DestaquesPage,
+    HomeTabPage,
+    MeuPerfilPage,
+    VocePage,
+    ContatoFutevoleiPage,
+    InformaEsDeUsoPage,
+    LoginPage,
+    CadastroUsuarioPage
   ],
   providers: [
     StatusBar,
