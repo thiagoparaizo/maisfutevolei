66bfdbd4c74ccefc99d57e486762a016563c6343
Thiago Paraizo <thiagoparaizo@gmail.com>
2018-01-26 00:50:33 -0300

    comeco implementacao busca - listagem

--- src/pages/busca/busca.ts
+++ src/pages/busca/busca.ts
@@ -1,6 +1,10 @@
+import { Quadra } from './../../objetos/quadra';
+import { Observable } from 'rxjs/Observable';
+import { QuadraRachaProvider } from './../../providers/quadra-racha';
 import { UtilProvider } from './../../providers/util';
 import { Component } from '@angular/core';
 import { NavController, LoadingController, ToastController } from 'ionic-angular';
+import { AngularFireList } from 'angularfire2/database';
 
 @Component({
   selector: 'page-busca',
@@ -8,14 +12,18 @@ import { NavController, LoadingController, ToastController } from 'ionic-angular
 })
 export class BuscaPage extends UtilProvider{
 
+  listaQuadras: Observable<any[]>;
+  listaRachas: Observable<any[]>;
+
   constructor(public navCtrl: NavController, private utilCrtl: UtilProvider,
-     public loadingCtrl: LoadingController, public toastCtrl: ToastController) {
+     public loadingCtrl: LoadingController, public toastCtrl: ToastController,
+     private duadraRachaProvider: QuadraRachaProvider) {
     super(loadingCtrl, toastCtrl);
   }
 
   doRefresh(refresher) {
     console.log('Begin async operation', refresher);
-
+    this.buscarAll();
     setTimeout(() => {
       console.log('Async operation has ended');
       refresher.complete();
@@ -24,6 +32,12 @@ export class BuscaPage extends UtilProvider{
 
   doPulling(refresher){
     console.log('doPulling....');
+  }
+
+  buscarAll(){
+    console.log('buscarAll >>');
+    this.listaQuadras = this.duadraRachaProvider.listarAllQuadras();
+    this.listaRachas = this.duadraRachaProvider.listarAllRachas();
   }
   
   
--- src/providers/quadra-racha.ts
+++ src/providers/quadra-racha.ts
@@ -1,6 +1,9 @@
+import { UsuarioProvider } from './usuario';
+import { FIREBASE_URL } from './../environments/firebase-url';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { UserProfile } from '../objetos/user-profile';
 
 /*
   Generated class for the QuadraRachaProvider provider.
@@ -11,17 +14,33 @@ import { AngularFireDatabase } from 'angularfire2/database';
 @Injectable()
 export class QuadraRachaProvider {
 
-  listaQuadars: Observable<any>;
+  listaQuadars: Observable<any[]>;
   
-  constructor(private afDataBase: AngularFireDatabase) {
+  constructor(private afDataBase: AngularFireDatabase, private usuarioProvider: UsuarioProvider) {
     console.log('Hello QuadraRachaProvider Provider');
   }
 
-  listarQuadras(){
+  listarAllQuadras(){
+    console.log("listarAllQuadras >>");
+    let url = FIREBASE_URL.PATH_LISTA_QUADRAS_ESTADO.replace(/{estado}/gi, "CE");
+    console.log("url: "+ url);
+    return this.afDataBase.list(url).valueChanges();
+  }
+
+  listarAllRachas(){
+    console.log("listarAllQuadras >>");
+    let url = FIREBASE_URL.PATH_LISTA_RACHAS_ESTADO.replace(/{estado}/gi, "CE");
+    console.log("url: "+ url);
+    return this.afDataBase.list(url).valueChanges();
+  }
+
+
+
+  listarFavoritosQuadras(){
 
   }
 
-  listaRachas(){
+  listarFavoritosRachas(){
 
   }
 
